f4839526e4cf025b7dc3c53718cdcef8c6a09c99
TheerthaPraveen <theerthapraveen.n@gmail.com>
2022-07-06 23:34:57 +0530

    payment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SearchRestaurantComponent } from './search-restaurant/search-restaurant.component';
 import { ViewFoodComponent } from './view-food/view-food.component';
+import { AddPaymentComponent } from './add-payment/add-payment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
     SearchRestaurantComponent,
-    ViewFoodComponent
+    ViewFoodComponent,
+    AddPaymentComponent
   ],
   imports: [
     BrowserModule,
